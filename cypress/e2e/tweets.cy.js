48035b209d496019c33195433dd6c3d4d1ff5795
santytorrico <90476969+santytorrico@users.noreply.github.com>
2022-11-24 09:23:43 -0400

    testing tweet button

--- cypress/e2e/tweets.cy.js
+++ cypress/e2e/tweets.cy.js
@@ -2,5 +2,6 @@ describe("Tweetea", () => {
     it("deberia interactuar con el input field", () => {
     cy.visit('http://localhost:1234');
      cy.get("#tweetText").type("hola mundo");
+     cy.get("#tweetButton").click();
     });
 });
--- src/tweetPresenter.js
+++ src/tweetPresenter.js
@@ -4,6 +4,7 @@ let tweetBtn =document.querySelector("#tweetButton")
 function addTexttoTweet(){
     tweetBtn.addEventListener("click", (event) => {
         event.preventDefault();
+       
         div.innerHTML = "<p>" + tweetTxt.value + "</p>";
         console.log(tweetTxt.value);
       });
