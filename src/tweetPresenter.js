4d530aa02d98476a3af6d03aa3e4d95fd10a10fa
santytorrico <90476969+santytorrico@users.noreply.github.com>
2022-11-24 09:20:23 -0400

    added tweetbtn

---
+++ src/tweetPresenter.js
@@ -0,0 +1,11 @@
+let tweetTxt =document.querySelector("#tweetText");
+let tweetBtn =document.querySelector("#tweetButton")
+
+function addTexttoTweet(){
+    tweetBtn.addEventListener("click", (event) => {
+        event.preventDefault();
+        div.innerHTML = "<p>" + tweetTxt.value + "</p>";
+        console.log(tweetTxt.value);
+      });
+}
+  
